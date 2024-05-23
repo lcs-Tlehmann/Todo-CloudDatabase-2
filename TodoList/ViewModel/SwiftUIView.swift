07f20aee1a9cecae1c2a8dd63e27197f03c75dd5
Tristan Lehmann <tlehmann@lcs.on.ca>
2024-05-23 09:59:06 -0400

    progress done

---
+++ TodoList/ViewModel/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  TodoList
+//
+//  Created by Tristan Lehmann on 2024-05-23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
